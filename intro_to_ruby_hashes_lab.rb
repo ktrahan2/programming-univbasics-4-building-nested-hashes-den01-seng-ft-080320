6f18b26649dd3a676c5fdeaa39cd8e7995e8d0d6
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-17 15:47:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def monopoly_with_second_tier
   newhash = {:railroads => {}}
-  newhash[:railroads] >> {:pieces => "4"}
+  newhash[:railroads][{}] >> {:pieces => "4"}
   return newhash
 
 end
